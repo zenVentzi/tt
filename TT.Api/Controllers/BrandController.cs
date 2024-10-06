6e7e239567c81a6c5408077ee231fca43f168334
zen-ventzi-marinov <ventzislav.marinov@gmail.com>
2024-10-06 12:59:25 +0300

    BrandController Export endpoint based on productId

--- TT.Api/Controllers/BrandController.cs
+++ TT.Api/Controllers/BrandController.cs
@@ -7,16 +7,35 @@ using TT.Lib.Mvc;
 namespace TT.Api.Controllers
 {
     [Route("[controller]")]
-    public class BrandController :  ReadWriteController<Brand, int>
+    public class BrandController : ReadWriteController<Brand, int>
     {
         private readonly IBrandService mainService;
+        private readonly IProductService productService;
 
-        public BrandController(IBrandService mainService) : base((IReadWriteService<Brand>)mainService)
+        public BrandController(IBrandService mainService, IProductService productService) : base((IReadWriteService<Brand>)mainService)
         {
             this.mainService = mainService;
+            this.productService = productService;
         }
 
-        [HttpDelete]
-        public new async Task<IActionResult> Delete(Brand brand) => Ok(await base.Delete(brand));
-    }
+        public async Task<IActionResult> Export(string? productKey)
+        {
+            try
+            {
+                if (productKey == null) 
+                { 
+                    return Ok(await mainService.Export());
+                }
+
+                return Ok(await this.productService.ExportProduct(productKey));
+            }
+            catch (System.Exception e)
+            {
+                // log exception
+                return StatusCode(500, "Something went wrong. Please check admin or server logs for more info.");
+            }
+        }
+    [HttpDelete]
+    public new async Task<IActionResult> Delete(Brand brand) => Ok(await base.Delete(brand));
+}
 }
